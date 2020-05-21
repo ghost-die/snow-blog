ff74bda8a451cf5f55fb71dae3552f52933bc2b7
Ghost <wq1436@hotmail.com>
2020-05-22 03:38:34 +0800

    README.md

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta name="keywords" content="博客,bogl,blog,{{ config('app.name', 'Laravel') }},孤城,雪,ghost">
+    <meta name="description" content="一人，一剑，守一座孤城，等你归来" />
     <title>{{ config('app.name', 'Laravel') }} -- {{$title ?? ""}}</title>
 
 
