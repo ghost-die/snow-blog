d734bdf94ea1339ab8c1d5a63f80328bbb149d05
Ghost <wq1436@hotmail.com>
2020-05-26 00:38:53 +0800

    add /t 测试文件

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Storage;
 */
 
 Route::get('/t',function (\Illuminate\Http\Request $request){
-	return $request->getClientIp();
+	return $request->getClientIps();
 });
 
 Auth::routes();
