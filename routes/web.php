0858fc9b03a14c9b814d70257f3cde83dd39ddb5
Ghost <wq1436@hotmail.com>
2020-05-26 01:14:04 +0800

    add /t 测试文件

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Storage;
 */
 
 Route::get('/t',function (\Illuminate\Http\Request $request){
-	return $_SERVER['HTTP_X_FORWARDED_FOR'];
+	return $_SERVER;
 });
 
 Auth::routes();
