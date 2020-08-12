1d608affa31dccab7b5e954700a1c5c828591033
Ghost <wq1436@hotmail.com>
2020-08-13 00:12:50 +0800

    update:adminlte3.0 push version 1.01

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Storage;
 */
 
 Route::get('/t',function (\Illuminate\Http\Request $request){
-	return $request->server();
+	dd($request->server()) ;
 });
 
 Auth::routes();
